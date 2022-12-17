9b87ea049d70cb8d6a10a9c77659f9b0121a810a
Parham Izadyar <77784179+prhmi@users.noreply.github.com>
2022-12-17 20:44:55 +0330

    Create kimia.bat

---
+++ kimia.bat
@@ -0,0 +1,19 @@
+@echo off
+set/p input= type the password please: 
+
+if NOT %input%== fishtoo goto :FAIL
+msg * that's right. let's see your gift
+msg *  \\\hugs// deep and vooshi
+msg * i put a digital hug in there, so If the world changes, this feeling will remain for us
+msg * now close your eyes and pick a happy memory
+msg * okay, i'm in your head now
+msg * and in your heart
+msg * and i will always stay here with you
+msg * now make a wish
+msg * happy birthday
+goto :end
+:FAIL
+msg * No! that's not the password. search it in your heart...
+
+:end
+
